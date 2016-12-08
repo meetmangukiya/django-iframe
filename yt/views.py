36a81ff0ee3d2eae8929d0af38842570876e1627
Meet Mangukiya <meet123mangukiya@gmail.com>
2016-12-08 14:39:58 +0530

    yt/views: Add `play` view and adapt the template
    Closes https://github.com/meetmangukiya/iframe-2.0/issues/3
    Closes https://github.com/meetmangukiya/iframe-2.0/issues/7

--- yt/views.py
+++ yt/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+def play(request):
+    """
+    View function for the `/play` endpoint.
+    """
+    # Setting variables
+    ids = request.GET.getlist("id", [])
+    title = None
+
+    # Constructing the context
+    context = {
+        "ids": ids,
+        "title": title if title else "FieryBit | yTif",
+    }
+
+    return render(request, 'yt/iframe.html', context)
