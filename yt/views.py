e5b2a8529f92f432891baed1801be6b3d02720d3
Meet Mangukiya <meet123mangukiya@gmail.com>
2016-12-12 01:00:15 +0530

    yt/views: Make a set of ids to prevent duplication
    Closes https://github.com/meetmangukiya/iframe-2.0/issues/21

--- yt/views.py
+++ yt/views.py
@@ -6,7 +6,7 @@ def play(request):
     View function for the `/play` endpoint.
     """
     # Setting variables
-    ids = request.GET.getlist("id", [])
+    ids = set(request.GET.getlist("id", []))
     if ids == []:
         return HttpResponse("You seem to have been stumbled upon a website that is " + 
                             "probably not for you unless you were invited too! " +
