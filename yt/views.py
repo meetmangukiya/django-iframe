aebc52095cb6f007919260d5e0182b23a323b0e5
Meet Mangukiya <meet123mangukiya@gmail.com>
2017-01-01 17:28:16 +0000

    Revert "yt/views, iframe: Allow playback quality change through url parameter"
    This reverts commit 0c45e704c3d8e648d2101ff8d4c82f5f5df3bc8a.

--- yt/views.py
+++ yt/views.py
@@ -5,19 +5,8 @@ def play(request):
     """
     View function for the `/` endpoint.
     """
-    supported_quality = {
-        'low': 'small',
-        'medium': 'medium',
-        'high': 'large',
-        'hd': 'hd720',
-        'hq': 'hd1080',
-        'hr': 'highres'
-        'default': 'default'
-    }
-
     # Setting variables
     ids = set(request.GET.getlist("id", []))
-    quality = request.GET.get("quality", "default")
     if not ids:
         return HttpResponse("You seem to have been stumbled upon a website that is " +
                             "probably not for you unless you were invited too! " +
@@ -28,7 +17,6 @@ def play(request):
     context = {
         "ids": ids,
         "title": title if title else "FieryBit | yTif",
-        "quality": supported_quality[quality]
     }
 
     return render(request, 'yt/iframe.html', context)
